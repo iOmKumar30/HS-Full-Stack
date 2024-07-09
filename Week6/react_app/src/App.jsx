82414c33f3af7e8bd494cba324ced2c925be3e54
iOmKumar30 <iomkumar03@outlook.com>
2024-07-10 00:31:39 +0530

    Understanding useEffect() hook

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -1,47 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  function CardWrapper({ children }) {
-    return (
-      <div
-        style={{
-          border: "1px solid black",
-          padding: "10px",
-          margin: "10px",
-          maxWidth: "600px",
-          maxheight: "300px",
-          textAlign: "center",
-          boxShadow:
-            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-          borderRadius: "5px",
-          fontFamily: "Arial, sans-serif",
-          fontSize: "30px",
-          fontWeight: "bold",
-        }}
-      >
-        {children}
-      </div>
-    );
-  }
+  const [todos, setTodos] = useState([]);
+
+  // the below setInterval will fetch the todos in every 5 seconds starting from the first time we run the app
+
+  /*   useEffect(() => {
+    setInterval(async () => {
+      try {
+        const response = await axios.get(
+          "https://sum-server.100xdevs.com/todos"
+        );
+        setTodos(response.data.todos);
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    }, 5000);
+  }, []); */
+
+  // if we want to fetch the todos as soon as we run the app, and then send further requests in every 5 seconds, we can do the below
+
+  useEffect(() => {
+    async function fetchTodos() {
+      const response = await axios.get("https://sum-server.100xdevs.com/todos");
+      setTodos(response.data.todos);
+    }
+
+    fetchTodos();
+
+    const interval = setInterval(fetchTodos, 5000);
+
+    return () => clearInterval(interval);
+  },[]);
 
   return (
     <div>
-      <CardWrapper>Hi there 1</CardWrapper>
-      <CardWrapper>Hi there 2</CardWrapper>
-      <CardWrapper>Hi there 3</CardWrapper>
-      <CardWrapper>Hi there 4</CardWrapper>
-      <CardWrapper>
-        <Hello name="Om" />
-      </CardWrapper>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+          />
+        ))}
+      </ul>
     </div>
   );
 
-  function Hello({ name }) {
-    return <h1>Hello {name}</h1>;
+  function Todo({ title, description }) {
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+      </div>
+    );
   }
 }
 
