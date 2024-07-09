4ba9eaf09b80b62c73571363b88c5fa99c8fa395
iOmKumar30 <iomkumar03@outlook.com>
2024-07-09 19:58:15 +0530

    reduced re-renders by using React.memo()

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("Header 1");
+  function updateTitle() {
+    setTitle(Math.random());
+  }
   return (
     <div>
       {/* we can put an empty <> instead of <div> as parent element */}
-      <HeaderWithButton />
+      <button onClick={updateTitle}>Update the title</button>
+      <br /><br />
+      <Header title={title} />
       <Header title="Header 2" />
       <Header title="Header 3" />
       <Header title="Header 4" />
@@ -16,22 +23,12 @@ function App() {
   );
 }
 
-function HeaderWithButton() {
-  const [title, setTitle] = useState("Header 1");
+const Header = React.memo(function ({ title }) {
   return (
     <div>
-      <button onClick={updateTitle}>Update the title</button>
-      <br /> <br /> <br />
-      <Header title={title} />
+      I am {title}
     </div>
-  );
-  function updateTitle() {
-    setTitle(Math.random());
-  }
-}
-
-function Header({ title }) {
-  return <div>I am {title}</div>;
-}
+  )
+})
 
 export default App;
