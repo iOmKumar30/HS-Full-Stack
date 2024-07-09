96ac279f3144109ed6470458a4b3ffa1b4c4a0ec
iOmKumar30 <iomkumar03@outlook.com>
2024-07-09 23:07:45 +0530

    Actual Wrapper Syntax

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -5,43 +5,44 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  function TextComponent({ str }) {
+  function CardWrapper({ children }) {
     return (
-      <div>
-        <h1>{str}</h1>
-      </div>
-    );
-  }
-
-  /* Here, componentProps is passed as an object containing the prop str that the TextComponent expects. The spread operator {...componentProps} is used to pass all the properties in componentProps as props to Component. */
-  function CardWrapper({ Component, componentProps }) {
-    return (
-      <div style={{ border: "1px solid black", padding: "10px", margin: "10px", maxWidth: "300px", maxheight: "300px", textAlign: "center", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", borderRadius: "5px", fontFamily: "Arial, sans-serif" }}>
-        <Component {...componentProps} />
+      <div
+        style={{
+          border: "1px solid black",
+          padding: "10px",
+          margin: "10px",
+          maxWidth: "600px",
+          maxheight: "300px",
+          textAlign: "center",
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          borderRadius: "5px",
+          fontFamily: "Arial, sans-serif",
+          fontSize: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        {children}
       </div>
     );
   }
 
   return (
     <div>
-      <CardWrapper
-        Component={TextComponent}
-        componentProps={{ str: "Hello World" }}
-      />
-      <CardWrapper
-        Component={TextComponent}
-        componentProps={{ str: "Hello World 2" }}
-      />
-      <CardWrapper
-        Component={TextComponent}
-        componentProps={{ str: "Hello World 3" }}
-      />
-      <CardWrapper
-        Component={TextComponent}
-        componentProps={{ str: "Hello World 4" }}
-      />
+      <CardWrapper>Hi there 1</CardWrapper>
+      <CardWrapper>Hi there 2</CardWrapper>
+      <CardWrapper>Hi there 3</CardWrapper>
+      <CardWrapper>Hi there 4</CardWrapper>
+      <CardWrapper>
+        <Hello name="Om" />
+      </CardWrapper>
     </div>
   );
+
+  function Hello({ name }) {
+    return <h1>Hello {name}</h1>;
+  }
 }
 
 export default App;
