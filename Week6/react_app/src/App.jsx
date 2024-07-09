73536fadb057c074bb0851a7201da73c858414e4
iOmKumar30 <iomkumar03@outlook.com>
2024-07-09 21:27:52 +0530

    key property in react and why it is important

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -5,30 +5,56 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [title, setTitle] = useState("Header 1");
-  function updateTitle() {
-    setTitle(Math.random());
-  }
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Go to gym",
+      description: "Go to gym from 7-9",
+    },
+    {
+      id: 2,
+      title: "Study DSA",
+      description: "Study DSA form 9-10",
+    },
+    {
+      id: 3,
+      title: "Cook dinner",
+      description: "Cook chicken rice for dinner",
+    },
+  ]);
+
   return (
     <div>
-      {/* we can put an empty <> instead of <div> as parent element */}
-      <button onClick={updateTitle}>Update the title</button>
-      <br /><br />
-      <Header title={title} />
-      <Header title="Header 2" />
-      <Header title="Header 3" />
-      <Header title="Header 4" />
-      <Header title="Header 5" />
+      <h1>All Todos</h1>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
+      <br />
+      <h1>Add Todo</h1>
+      {/* Everytime the button is clicked a new random TODO will be added */}
+      <button onClick={addTodo}>Tap to Add</button>
+    </div>
+  );
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        title: Math.random().toString(36).substring(3),
+        description: Math.random().toString(36).substring(3),
+      },
+    ]);
+  }
+}
+
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h5>{description}</h5>
     </div>
   );
 }
-
-const Header = React.memo(function ({ title }) {
-  return (
-    <div>
-      I am {title}
-    </div>
-  )
-})
 
 export default App;
