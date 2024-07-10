46c15edea4116dab12f26e6abca9772eb2219d7c
iOmKumar30 <iomkumar03@outlook.com>
2024-07-10 17:47:25 +0530

    Did a simple todo assignment with multiple times  fetching data using useEffect()

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -3,62 +3,47 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 
+/* Task:
+* Create a component that takes a todo id as input 
+* And renders it by fetching it from the server 
+* The parent component should have a button, clicking on which the next 
+todo is fetched */
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  // the below setInterval will fetch the todos in every 5 seconds starting from the first time we run the app
-
-  /*   useEffect(() => {
-    setInterval(async () => {
-      try {
-        const response = await axios.get(
-          "https://sum-server.100xdevs.com/todos"
-        );
-        setTodos(response.data.todos);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    }, 5000);
-  }, []); */
-
-  // if we want to fetch the todos as soon as we run the app, and then send further requests in every 5 seconds, we can do the below
-
-  useEffect(() => {
-    async function fetchTodos() {
-      const response = await axios.get("https://sum-server.100xdevs.com/todos");
-      setTodos(response.data.todos);
-    }
-
-    fetchTodos();
-
-    const interval = setInterval(fetchTodos, 5000);
-
-    return () => clearInterval(interval);
-  },[]);
-
+  const [id, setId] = useState(1);
+  if (id == 6) {
+    setId(1);
+  }
   return (
     <div>
-      <h1>Todos</h1>
-      <ul>
-        {todos.map((todo) => (
-          <Todo
-            key={todo.id}
-            title={todo.title}
-            description={todo.description}
-          />
-        ))}
-      </ul>
+      <button onClick={() => setId(id + 1)}>Next</button>
+      <Todo id={id} />
     </div>
   );
+}
 
-  function Todo({ title, description }) {
-    return (
-      <div>
-        <h1>{title}</h1>
-        <h3>{description}</h3>
-      </div>
-    );
-  }
+function Todo({ id }) {
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try {
+        const response = await axios.get(
+          `https://sum-server.100xdevs.com/todo?id=${id}`
+        );
+        setTodo(response.data.todo);
+      } catch (error) {
+        console.error("Error fetching the todo:", error);
+      }
+    };
+
+    fetchTodo();
+  }, [id]);
+  return (
+    <div>
+      <h2>{todo.title}</h2>
+      <h5>{todo.description}</h5>
+    </div>
+  );
 }
 
 export default App;
