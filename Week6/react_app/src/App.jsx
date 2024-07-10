0149d394c62a82c7e82750dd0312e044d58db6b8
iOmKumar30 <iomkumar03@outlook.com>
2024-07-11 01:10:03 +0530

    a simple task

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -1,51 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import React from "react";
 import axios from "axios";
 import "./App.css";
 
 /* Task:
-* Create a component that takes a todo id as input 
-* And renders it by fetching it from the server 
-* The parent component should have a button, clicking on which the next 
-todo is fetched */
+Create an app that has two components:
+* an input box that takes a number and renders sum of 1 to that number
+* a counter that increments normally on clicking but also increments whenever there is a change in the input box\
+ */
 function App() {
-  const [id, setId] = useState(0);
-
+  const [counter, setCounter] = useState(0);
+  const [num, setNum] = useState(0);
   return (
     <div>
-      <button onClick={() => setId(1)}>1</button>
-      <button onClick={() => setId(2)}>2</button>
-      <button onClick={() => setId(3)}>3</button>
-      <button onClick={() => setId(4)}>4</button>
-      <button onClick={() => setId(5)}>5</button>
+      <input
+        type="text"
+        onChange={(e) => {
+          setNum(e.target.value);
+          setCounter(counter + 1);
+        }}
+      />
       <br />
-      id : {id}
-      {id > 0 && <Todo id={id} />}
-    </div>
-  );
-}
-
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    const fetchTodo = async () => {
-      try {
-        const response = await axios.get(
-          `https://sum-server.100xdevs.com/todo?id=${id}`
-        );
-        setTodo(response.data.todo);
-      } catch (error) {
-        console.error("Error fetching the todo:", error);
-      }
-    };
-
-    fetchTodo();
-  }, [id]);
-  return (
-    <div>
-      <h2>{todo.title}</h2>
-      <h5>{todo.description}</h5>
+      Sum is {(parseInt(num) * (parseInt(num) + 1)) / 2} <br />
+      <button onClick={() => setCounter(counter + 1)}>Counter {counter}</button>
     </div>
   );
 }
