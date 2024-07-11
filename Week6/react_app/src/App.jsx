f8f49f4377d0cbfd92ed76ee6ab240b76b90e23e
iOmKumar30 <iomkumar03@outlook.com>
2024-07-12 00:32:30 +0530

    learn useCallback()

--- Week6/react_app/src/App.jsx
+++ Week6/react_app/src/App.jsx
@@ -1,61 +1,33 @@
-import { useState, useMemo } from "react";
+import { memo, useCallback, useState, useMemo } from "react";
 import React from "react";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [num, setNum] = useState(0);
+  /* 
+  var a = useCallback(() => {
+    console.log("a");
+  }, []);
+ */
+  /*  var a = {
+    sum: 1
+  } */
 
-  const sum = useMemo(() => {
-    return (num * (num + 1)) / 2;
-  }, [num]);
-
+  /* un-comment each to see the difference on console */
+  var a = useCallback(() => {
+    sum: 100;
+  }, []);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter number to calculate sum"
-        style={{
-          width: "300px",
-          height: "30px",
-          fontSize: "20px",
-          padding: "5px",
-          margin: "10px",
-          borderRadius: "5px",
-        }}
-        onChange={(e) => {
-          const value = parseInt(e.target.value);
-          if (!isNaN(value)) {
-            setNum(value);
-            setCounter(counter + 1);
-          }
-        }}
-      />
-      <br />
-      {num > 0 && (
-        <>
-          <span style={{ fontSize: "20px", margin: "10px" }}>
-            Sum of 1 to {num} is : {sum}
-          </span>
-          <br />
-        </>
-      )}
-      <button
-        style={{
-          fontSize: "20px",
-          padding: "5px",
-          margin: "10px",
-          borderRadius: "5px",
-          cursor: "pointer",
-          backgroundColor: "magenta",
-          color: "white",
-        }}
-        onClick={() => setCounter(counter + 1)}
-      >
-        Counter {counter}
-      </button>
+      <button onClick={() => setCounter(counter + 1)}>Counter {counter}</button>
+
+      <Demo a={a} />
     </div>
   );
 }
+const Demo = memo(({ a }) => {
+  console.log("Re-rendered");
+  return <div>Hi there</div>;
+});
 
 export default App;
